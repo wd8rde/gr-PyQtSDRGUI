369f13921343f9e610c54148da97c9fd81bb67c8
robert.bouterse <robert.bouterse@soundunited.com>
2020-02-10 10:03:46 -0500

    Solved PyQt4/PyQt5 compatiblity, now works on GRC 3.7.9, and 3.7.13

--- python/gr_PyQtSDRGUI.py
+++ python/gr_PyQtSDRGUI.py
@@ -35,8 +35,13 @@ if('PyQt5' in sys.modules.keys()):
     from PyQt5 import QtCore, QtGui, QtWidgets, uic
     from PyQt5.QtWidgets import QWidget
     from PyQt5.QtWidgets import QApplication
+elif('PyQt4' in sys.modules.keys()):
+    print('using PyQt5')
+    from PyQt4 import Qt, QtCore, QtGui, uic
+    from PyQt4.Qt import QWidget
+    from PyQt4.Qt import QApplication
 else:
-    sys.stderr.write("Error: Program requires PyQt5.\n")
+    sys.stderr.write("Error: Program requires PyQt5, or PyQt4.\n")
     sys.exit(1)
 
 from OpenSdrGuiLogic import OpenSdrGuiLogic
